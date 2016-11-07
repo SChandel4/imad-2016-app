ebe05cc4c8324ce97ed9a272a6089bc7b60fdc10
SChandel4 <suryansh.chandel004@gmail.com>
2016-11-08 00:46:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/ui/s-icon.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 's-icon.png'));
 });
 
+app.get('/ui/bsnl.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bsnl.png'));
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
