2abb5c5b43ca75e5876e9ed477234432ff8ff06a
SChandel4 <suryansh.chandel004@gmail.com>
2016-11-08 00:50:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,6 +25,33 @@ app.get('/ui/bsnl.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bsnl.png'));
 });
 
+app.get('/ui/aaruush.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'aaruush.png'));
+});
+
+app.get('/ui/sof.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'sof.png'));
+});
+
+app.get('/ui/Facebook-icon.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Facebook-icon.png'));
+});
+
+app.get('/ui/Google-Plus-icon.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Google-Plus-icon.png'));
+});
+
+app.get('/ui/Linkedin-icon.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Linkedin-icon.png'));
+});
+
+app.get('/ui/Github-icon.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Github-icon.png'));
+});
+
+app.get('/ui/Website-icon.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Website-icon.png'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
